2dea2bb48412ed691f6e4bbd4577b57caea0b973
Azu <96851229+Azu-kitsu@users.noreply.github.com>
2024-02-23 21:40:29 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+
+#include <vector>
+#include <set>
+#include <deque>
+#include <map>
+#include <string>
+
+#include <algorithm>
+#include <cstdlib> 
+#include <iostream>
+
+void random(int lb, int ub, int amount)
+{
+    // This program will create some sequence of random 
+    // numbers on every program run within range lb to ub 
+    for (int i = 0; i < amount; i++)
+        cout << (rand() % (ub - lb + 1)) + lb << " ";
+}
+
+typedef long long ll;
+typedef vector<int> vInt;
+typedef vector<vector<int>> vvInt;
+typedef vector<string> vString;
+typedef pair<int, int> Point;
+
+//if ((r1 < l2) || (r2 < l1)) then no intersection exits.
+//else l = max(l1, l2) and r = min(r1, r2)
+
+int main()
+{
+
+}
